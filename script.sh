24a653e2a01c515e124991fb0d8c2f22aede8b28
Librake <ya.snake54@ya.ru>
2022-09-22 01:11:36 +0300

    init

---
+++ script.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# коментус
+
+name="Glib"
+str="Имя юзера: "
+echo "$str $name "
+
+mydir=`pwd`
+mydir2=$(pwd)
+echo "расположение $mydir2"
+
+num1=34
+num2=42
+result=$(($num1 + $num2))
+echo "$result"
