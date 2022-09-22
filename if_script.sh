db113d2d7084e7fcdc1854437cc421eccda1d2f8
Librake <ya.snake54@ya.ru>
2022-09-22 14:29:51 +0300

    add if_script code

--- if_script.sh
+++ if_script.sh
@@ -1,2 +1,26 @@
 #! /bin/bash
 
+user_name=user
+if grep $user_name /etc/passwd
+then
+echo "Юзер найден"
+cd /home
+ls
+else
+echo "Юзера нема"
+fi
+
+num1=9
+num2=7
+if [ $num1 -eq $num2 ]
+then
+echo "equal"
+elif [ $num1 -gt $num2 ]
+then
+echo "num1 > num2"
+elif [ $num1 -lt $num2 ]
+then
+echo "num1 < num2"
+else
+echo "dich"
+fi
